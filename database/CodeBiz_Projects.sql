e6265ca6461156cd0d4e8297c3add3ee6c16883b
Arlyn <musadaarlyn@gmail.com>
2025-11-29 22:45:19 +0800

    Initial Commit

---
+++ database/CodeBiz_Projects.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE codebiz_projects;
+USE codebiz_projects;
+
+-- Tech stack category table
+CREATE TABLE tech_stack_category (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    tscName VARCHAR(255) NOT NULL UNIQUE,
+    tscDescription TEXT,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tech stack table
+CREATE TABLE tech_stack (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    tsName VARCHAR(255) NOT NULL,
+    tsDescription TEXT,
+    categoryId BIGINT,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (categoryId) REFERENCES tech_stack_category(id) ON DELETE SET NULL
+);
+
+-- Projects table
+CREATE TABLE projects (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    projName VARCHAR(255) NOT NULL,
+    projDescription TEXT,
+    techStackIds JSON,
+    status ENUM('Planning', 'In Progress', 'Completed', 'On Hold') DEFAULT 'Planning',
+    startDate DATE,
+    endDate DATE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
