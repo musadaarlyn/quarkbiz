baa51e18f0b36e96df3d6846b4b8ce42fb455dac
bananarlyn <musadaarlyn@gmail.com>
2025-12-10 10:13:53 +0800

    Frontend Architecture Plan

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,16 +1,7 @@
-import Button from "./components/Button.tsx";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-50">
-      <h1 className="text-3xl font-bold text-gray-800">
-        Tailwind + React + TypeScript is Working! 🚀
-      </h1>
-
-      <Button onClick={() => alert("Button clicked!")}>
-        Click Me
-      </Button>
-    </div>
+   <div></div>
   );
 }
 
